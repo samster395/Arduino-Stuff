ef86e89364f6478bbfbe1e3bd0dbbf998f167973
ToXIc <4998146+ToXIc-Dev@users.noreply.github.com>
2019-10-16 11:49:01 +0100

    Update timelapse.bat

--- ESP32 Cam/timelapse/timelapse.bat
+++ ESP32 Cam/timelapse/timelapse.bat
@@ -1,7 +1,7 @@
 ECHO OFF
-set /p fold=What folder are the photos in (no traling slash)?:
+set /p fold=What folder are the photos in (no trailing slash)?:
 set /p of=Output filename?:
 REM ECHO .\ffmpeg -framerate %fr% -i %fold%\%%d.jpg -c:v libx264 %of%.mp4
-ffmpeg\bin\ffmpeg.exe -framerate 30 -i output\%fold%\%%d.jpg -c:v libx264 timelapse_output\%of%.mp4
+ffmpeg\bin\ffmpeg.exe -framerate 15 -i output\%fold%\%%d.jpg -c:v libx264 timelapse_output\%of%.mp4
 ECHO Finished
 PAUSE
