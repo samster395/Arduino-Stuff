07fa9584e4db1afe7c34a7b4c8dc6dbe83512f52
ToXIc-Dev <4998146+ToXIc-Dev@users.noreply.github.com>
2020-02-15 18:10:40 +0000

    Upload Timelapse2

---
+++ ESP32 Cam/timelapse/TimeLapse2/file.h
@@ -0,0 +1,8 @@
+#pragma once
+bool writeFile(const char *path, const unsigned char *data, unsigned long len);
+bool appendFile(const char *path, const unsigned char *data, unsigned long len);
+bool initFileSystem();
+bool createDir(const char *path);
+bool fileExists(const char *path);
+extern int sd_tot;
+extern int sd_used;
